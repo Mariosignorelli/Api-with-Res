3049fe1a973382715e50570adc76e35245e41717
Mario <signorelii2308@gmail.com>
2022-05-30 11:50:32 -0300

    atualizando projeto

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello word devs')
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request , res:Response , next:NextFunction) => {
+res.status(200).send({foo: 'bar'});
+});
+app.listen(3000, () =>{
+console.log('aplicação executando na porta 3000')
+});
