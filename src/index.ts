c6c7448184ece36455d44cc342358cf03aea4fa1
Mario <signorelii2308@gmail.com>
2022-05-31 16:11:13 -0300

    criando pasta routers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, {Request, Response, NextFunction} from 'express';
+import usersRoute from './routes/users.route';
 
 const app = express();
+app.use(usersRoute);
 
 app.get('/status', (req: Request , res:Response , next:NextFunction) => {
-res.status(200).send({foo: 'I love javascrispt  my  faverite language '});
+res.status(200).send({foo: 'I love javascrispt  my  faverite language hello word '});
 });
 app.listen(3000, () =>{
 console.log('aplicação executando na porta 3000')
---
+++ src/routes/users.route.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response, NextFunction } from 'express';
+// get / users
+// get / users/ uuid
+// post / users 
+//put /users /uuid
+// delete /users /uuid
+
+ const usersRoute = Router();
+
+ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction ) => {
+    const users = [{userName: 'Mario'}]
+    res.status(200).send({users})
+ })
+ export default usersRoute;
