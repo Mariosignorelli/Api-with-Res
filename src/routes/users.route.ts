21ae7eac6ed389531ca296ab23ebae68fa731206
Mario <signorelii2308@gmail.com>
2022-05-31 19:34:03 -0300

    segunda rota metodo request

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 // get / users
-// get / users/ uuid
+// get / users/uuid
 // post / users 
 //put /users /uuid
 // delete /users /uuid
@@ -10,5 +10,11 @@ import { Router, Request, Response, NextFunction } from 'express';
  usersRoute.get('/users', (req: Request, res: Response, next: NextFunction ) => {
     const users = [{userName: 'Mario'}]
     res.status(200).send({users})
- })
+ });
+
+ usersRoute.get('/users/uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction ) =>{
+   const uuid = req.params.uuid;
+
+   res.status(200).send({uuid});
+ });
  export default usersRoute;
