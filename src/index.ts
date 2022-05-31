c0630e5a7c68ac24946035509844fb25226bfb43
Mario <signorelii2308@gmail.com>
2022-05-31 15:21:37 -0300

    atualizando packege.JSON

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, {Request, Response, NextFunction} from 'express';
 const app = express();
 
 app.get('/status', (req: Request , res:Response , next:NextFunction) => {
-res.status(200).send({foo: 'bar'});
+res.status(200).send({foo: 'I love javascrispt  my  faverite language '});
 });
 app.listen(3000, () =>{
 console.log('aplicação executando na porta 3000')
