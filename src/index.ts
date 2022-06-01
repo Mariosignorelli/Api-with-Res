a5d41dd4c10d5ddc359486ff601fc6a3d1dc84ce
Mario <signorelii2308@gmail.com>
2022-06-01 12:31:47 -0300

    inciando metodo post

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import express, {Request, Response, NextFunction} from 'express';
 import usersRoute from './routes/users.route';
 
 const app = express();
+// configuração da aplicação
+app.use(express.json());
+// configuração da rota 
 app.use(usersRoute);
 
 app.get('/status', (req: Request , res:Response , next:NextFunction) => {
 res.status(200).send({foo: 'I love javascrispt  my  faverite language hello word '});
 });
+
+// inicializaçãodo servidor
 app.listen(3000, () =>{
 console.log('aplicação executando na porta 3000')
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
 // get / users
 // get / users/uuid
 // post / users 
@@ -9,12 +10,17 @@ import { Router, Request, Response, NextFunction } from 'express';
 
  usersRoute.get('/users', (req: Request, res: Response, next: NextFunction ) => {
     const users = [{userName: 'Mario'}]
-    res.status(200).send({users})
+    res.status(StatusCodes.OK).send({users})
  });
 
  usersRoute.get('/users/uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction ) =>{
    const uuid = req.params.uuid;
 
-   res.status(200).send({uuid});
+   res.status(StatusCodes.OK).send({uuid});
  });
+ usersRoute.post('/status', (req: Request, res: Response, next: NextFunction ) =>{
+   const newUser = req.body
+   console.log(req.body);
+   res.status(StatusCodes.CREATED).send(newUser);
+ })
  export default usersRoute;
